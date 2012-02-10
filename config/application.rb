d20a757adc5550f8ba92e0ad9141e48e1ca4d228
Jen Kelly <jenstrumental@gmail.com>
2012-02-10 02:43:37 -0800

    fix another config bug

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require "active_resource/railtie"
 require "rails/test_unit/railtie"
 require 'mongoid/railtie' 
 
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
